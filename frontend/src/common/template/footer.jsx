6b829363844dbc859983715b0352b7a811368413
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-07-30 18:32:32 -0300

    rodape

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default props => (
-    <footer className="main-footer">
-        <strong>
-            Copyright &copy; 2021
-            <a href='http://cod3r.com.br' target="_blank"> Cod3r</a>.
-        </strong>
-    </footer>
-)
+export default props => {
+    const date = new Date()
+    const year = date.getFullYear()
+
+    return (
+        <footer className="main-footer">
+            <strong>
+                Copyright &copy; {year}
+                <a href='http://cod3r.com.br' target="_blank"> Cod3r</a>.
+            </strong>
+        </footer>
+    )
+}
